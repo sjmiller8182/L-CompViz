fdc5948d8b4227573783bf20bf81445c86b41dce
Stuart Miller <stuart.j.miller.8182@gmail.com>
2018-12-15 17:40:59 -0600

    add pca, denoise

--- imtools.py
+++ imtools.py
@@ -7,6 +7,7 @@
     im_resize - resize an image (as np.array)
     equalize_hist - equalize histogram of image
     im_average - average a set of images
+    denoise - removes noise from and image
 """
 
 import glob
@@ -88,7 +89,6 @@ def im_average(im_paths, resize = None):
     
     """
     
-    
     if resize is not None:
         average_im = Image.open(im_paths[0]).resize(resize)
     else:
@@ -112,3 +112,73 @@ def im_average(im_paths, resize = None):
     average_im /= len(im_paths)
     
     return np.array(average_im, 'uint8')
+    
+def pca(X):
+    """
+    Compute averages of images in the list (greyscaled)
+    
+    im: list
+        a list of image paths
+    resize: tuple
+        resize images to a standard size
+    
+    Return: np.array
+        an average of images as an array
+    
+    """
+    
+    # get dimensions
+    num_data, dim = X.shape
+    
+    # center data
+    mean_X = X.mean(axis = 0)
+    X = X-mean_X
+    
+    # PCA
+    if dim > num_data:
+        M = np.dot(X,X.T)
+        e, ev = np.linalg.eigh(M)
+        tmp = np.dot(X.T, ev).T
+        V = tmp[::-1]
+        S = np.sqrt(e)[::-1]
+        for i in range(V.shape[1]):
+            V[:,i] /= S
+    else:
+        U, S, V = np.linalg.svd(X)
+        V = V[:num_data]
+    return V, S, mean_X
+    
+def denoise(im, U_init, tolerance = 0.1, tau = 0.125, tv_weight = 100):
+    
+    m, n = im.shape
+    
+    # init
+    U = U_init
+    Px = im
+    Py = im
+    error = 1
+    
+    while(error > tolerance):
+        Uold = U
+        
+        # calc grads
+        GradUx = np.roll(U,-1,axis = 0)
+        GradUy = np.roll(U,-1,axis = 1)
+        
+        PxNew = Px + (tau/tv_weight)*GradUx
+        PyNew = Py + (tau/tv_weight)*GradUy
+        NewNorm = np.maximum(1,np.sqrt(PxNew**2 + PyNew**2))
+        
+        Px = PxNew/NewNorm
+        Py = PyNew/NewNorm
+        
+        RxPx = np.roll(Px, 1, axis = 1)
+        RyPy = np.roll(Py, 1, axis = 0)
+        
+        DivP = (Px-RxPx)+(Py-RyPy)
+        
+        U = im + tv_weight*DivP
+        
+        error = np.linalg.norm(U-Uold)/np.sqrt(m*n)
+        
+    return U, im-U
